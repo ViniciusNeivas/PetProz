c1ed3fd308c66f44e00fc047555add6c8078db3f
albertomoiseis <contatoalbertomoiseis@gmail.com>
2023-02-12 23:31:58 -0300

    Alteração na validação!

--- js/validacao-login.js
+++ js/validacao-login.js
@@ -36,16 +36,25 @@ function mainPasswordValidate() {
   }
 }
 
+let elemento_pai1 = document.getElementById("elementoPai");
+let elemento_pai2 = document.getElementById("elementoPai");
+
 function validar() {
   if (!(!emailRegex.test(campos[0].value) && campos[1].value.length < 8)) {
-    var elemento_pai = document.body;
-    var titulo = document.createElement("h6");
-    titulo.textContent = "Formulário Enviado";
-    elemento_pai.appendChild(titulo);
+    validar1();
   } else {
-    var elemento_pai = document.body;
-    var titulo = document.createElement("h5");
-    titulo.textContent = "Formulário Não Enviado";
-    elemento_pai.appendChild(titulo);
+    validar2();
   }
+}
+function validar1() {
+  var titulo = document.createElement("span-h5");
+  titulo.textContent = "Formulário Enviado";
+  elemento_pai1.appendChild(titulo);
+  elemento_pai1.style.display = "block";
+}
+function validar2() {
+  var titulo = document.createElement("span-h6");
+  titulo.textContent = "Formulário precisa preencher campos corretamente.";
+  elemento_pai2.appendChild(titulo);
+  elemento_pai2.style.display = "block";
 }
